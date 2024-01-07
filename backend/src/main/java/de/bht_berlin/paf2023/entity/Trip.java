32e70002c682aecf10271139d1a0e179e3998b0f
Sven Urbach <s87412@teams.bht-berlin.de>
2024-01-07 16:04:57 +0100

    init faker

--- backend/src/main/java/de/bht_berlin/paf2023/entity/Trip.java
+++ backend/src/main/java/de/bht_berlin/paf2023/entity/Trip.java
@@ -10,7 +10,7 @@ import java.util.Date;
 import java.util.List;
 
 @Entity @Getter @Setter
-@Table(name = "trip", schema = "public")
+@Table(name = "trip")
 public class Trip extends IdentifiedEntity {
 
 //    @Id @GeneratedValue
