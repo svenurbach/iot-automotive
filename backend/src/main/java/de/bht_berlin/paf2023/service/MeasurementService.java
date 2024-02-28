31aa944c6f9b8eaadb4b13f49733a3a7d3fa5aee
Anica W <s88294@bht-berlin.de>
2024-02-28 19:30:15 +0100

    wip

--- backend/src/main/java/de/bht_berlin/paf2023/service/MeasurementService.java
+++ backend/src/main/java/de/bht_berlin/paf2023/service/MeasurementService.java
@@ -1,14 +1,19 @@
 package de.bht_berlin.paf2023.service;
 
 import de.bht_berlin.paf2023.entity.Measurement;
+import de.bht_berlin.paf2023.entity.measurements.*;
 import de.bht_berlin.paf2023.repo.MeasurementRepo;
+import de.bht_berlin.paf2023.repo.TripRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class MeasurementService {
+//    private final MeasurementRepo repository;
 
     private final MeasurementRepo measurementRepo;
 
@@ -18,6 +23,11 @@ public class MeasurementService {
     }
     // User Story: Messefehler/Inkonsistenzen erkennen
     // https://trello.com/c/1bafUTDM/37-user-story-messfehler-inkonsistenzen-erkennen
+
+//    @Autowired
+//    public MeasurementService(MeasurementRepo repository) {
+//        this.repository = repository;
+//    }
 
 //writeMetric(metric, value, vehicle, trip, )
 //    write single metric into db
@@ -35,11 +45,86 @@ public class MeasurementService {
 //        show error in measurement
 
     public String getMeasurements(long id) {
-        return "test";
-        // return repository.findById(id).get().get
+        return measurementRepo.findById(id).get().getMeasuredValue().toString();
     }
 
-    public List<Measurement> getMeasurementsByVehicle(long vehicleId) {
-        return measurementRepo.findByVehicle(vehicleId);
+
+    public ArrayList<Double> parseMeasurementArrayToDouble(Measurement[] measurementsArray) {
+        ArrayList<Double> measurementArrayList = new ArrayList<>();
+        for (Measurement measurement : measurementsArray) {
+            measurementArrayList.add(convertMeasurementToDouble(measurement));
+        }
+        return measurementArrayList;
+    }
+
+    private double convertMeasurementToDouble(Measurement measurement) {
+        switch (measurement.getMeasurementType()) {
+            case "AccelerationMeasurement":
+                return roundToTwoDecimalPlaces((double) ((AccelerationMeasurement) measurement).getAcceleration());
+            case "AxisMeasurement":
+                return roundToTwoDecimalPlaces((double) ((AxisMeasurement) measurement).getAxisAngle());
+            case "FuelMeasurement":
+                return roundToTwoDecimalPlaces((double) ((FuelMeasurement) measurement).getFuelLevel());
+            case "SpeedMeasurement":
+                return roundToTwoDecimalPlaces((double) ((SpeedMeasurement) measurement).getSpeed());
+            case "SteeringWheelMeasurement":
+                return roundToTwoDecimalPlaces((double) ((SteeringWheelMeasurement) measurement).getSteeringWheelAngle());
+//            case "TirePressureMeasurement":
+//                return roundToTwoDecimalPlaces((double) ((TirePressureMeasurement) measurement).getTirePressure());
+            default:
+                return 0.0;
+        }
+    }
+
+    private double roundToTwoDecimalPlaces(Double value) {
+        return Math.round(value * 100.0) / 100.0;
+    }
+
+
+    public double calculateAverageMeasurements(ArrayList<Double> measurementArrayList) {
+        double sum = 0;
+
+        for (Double measurement : measurementArrayList) {
+            sum += measurement;
+        }
+        if (!measurementArrayList.isEmpty()) {
+            double average = sum / measurementArrayList.size();
+            return roundToTwoDecimalPlaces(average);
+        } else {
+            return 0.0;
+        }
+    }
+
+    public boolean findMeasurementError(ArrayList<Double> measurementArrayInDouble, int comparativeValuesArraySize, Double tolerance) {
+        if (measurementArrayInDouble.size() < comparativeValuesArraySize) {
+            System.out.println("Nicht genügend Werte im Array, um Messfehler zu identifizieren");
+            return false;
+        }
+        boolean measurementError = true;
+        ArrayList<Double> comparativeValuesArrayPast = new ArrayList<>();
+        ArrayList<Double> comparativeValuesArrayFuture = new ArrayList<>();
+        for (int i = 0; i < measurementArrayInDouble.size(); i++) {
+            if (i < comparativeValuesArraySize) {
+                break;
+            } else {
+                while(comparativeValuesArrayPast.size() < comparativeValuesArraySize) {
+                    comparativeValuesArrayPast.add(measurementArrayInDouble.get(i));
+                }
+                double averagePast = calculateAverageMeasurements(comparativeValuesArrayPast);
+                if(averagePast <= measurementArrayInDouble.get(i) - (measurementArrayInDouble.get(i) * tolerance) ||
+                        averagePast >=measurementArrayInDouble.get(i) + (measurementArrayInDouble.get(i) * tolerance)){
+                    measurementError = false;
+                }
+                while (comparativeValuesArrayFuture.size() < comparativeValuesArraySize){
+                    comparativeValuesArrayFuture.add(measurementArrayInDouble.get(i+comparativeValuesArraySize+1));
+                }
+                double averageFuture = calculateAverageMeasurements(comparativeValuesArrayPast);
+                if(averageFuture <= measurementArrayInDouble.get(i) - (measurementArrayInDouble.get(i) * tolerance) ||
+                        averageFuture >=measurementArrayInDouble.get(i) + (measurementArrayInDouble.get(i) * tolerance)){
+                    measurementError = false;
+                }
+            }
+        }
+        return measurementError;
     }
 }
