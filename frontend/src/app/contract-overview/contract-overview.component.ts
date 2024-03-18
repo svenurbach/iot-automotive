d52fe25c36b8b8c8815b0d88519ae05b04f6e71a
Sven Urbach <urbachsven@gmail.com>
2024-03-18 12:19:59 +0100

    add person cars and contracts

--- frontend/src/app/contract-overview/contract-overview.component.ts
+++ frontend/src/app/contract-overview/contract-overview.component.ts
@@ -4,6 +4,8 @@ import { InsuranceService } from '../service/insurance.service';
 import {ContractDetailsComponent} from '../contract-details/contract-details.component';
 import {OsmViewComponent} from '../osm-view/osm-view.component';
 import {RouterLink} from "@angular/router";
+import {AppComponent} from "../app.component";
+import {BehaviorSubject, filter, Observable} from "rxjs";
 
 @Component({
   selector: 'app-contract-overview',
@@ -19,9 +21,27 @@ import {RouterLink} from "@angular/router";
 export class ContractOverviewComponent {
   contracts: Insurance[] = [];
   currentItem = 1;
+  policyholderSelection: BehaviorSubject<number> = new BehaviorSubject<number>(0);
 
-  constructor(private insuranceService: InsuranceService) {
-    this.getContracts();
+  constructor(private insuranceService: InsuranceService, private appComponent: AppComponent) {
+    this.policyholderSelection = this.appComponent._policyholderSelection;
+    this.appComponent._policyholderSelection.subscribe((personId: number) => {
+      if (personId == 0) { 
+        console.log("PersonId is 0")
+        this.getContracts();
+      } else {
+        this.getInsurancesByPerson(personId);
+      }
+    });
+  }
+
+  getInsurancesByPerson(personId: number): void {
+    this.insuranceService.getInsurancesByPerson(personId)
+      .subscribe((data) => {
+        this.contracts = data;
+        console.log(data)
+        console.log(this.contracts)
+      });
   }
 
   getContracts(): void {
--- frontend/src/app/vehicle/vehicle.component.ts
+++ frontend/src/app/vehicle/vehicle.component.ts
@@ -3,6 +3,8 @@ import {Vehicle} from "../model/vehicle.model";
 import {VehicleService} from "../service/vehicle.service";
 import {RouterLink} from "@angular/router";
 import {OsmViewComponent} from "../osm-view/osm-view.component";
+import {AppComponent} from "../app.component";
+import {BehaviorSubject, filter, Observable} from "rxjs";
 
 
 @Component({
@@ -13,20 +15,36 @@ import {OsmViewComponent} from "../osm-view/osm-view.component";
   styleUrl: './vehicle.component.css'
 })
 export class VehicleComponent {
-
   vehicles: Vehicle[] = [];
-
+  policyholderSelection: BehaviorSubject<number> = new BehaviorSubject<number>(0);
 
   ngOnInit(): void {
     // this.getAllMeasurementErrors();
   }
 
-  constructor(private vehicleService: VehicleService) {
-    this.getVehicles();
+  constructor(private vehicleService: VehicleService, private appComponent: AppComponent) {
+    this.policyholderSelection = this.appComponent._policyholderSelection;
+    this.appComponent._policyholderSelection.subscribe((personId: number) => {
+      if (personId == 0) { 
+        console.log("PersonId is 0")
+        this.getVehicles();
+      } else {
+        this.getCarsByPerson(personId);
+      }
+    });
   }
 
   getVehicles(): void {
     this.vehicleService.getVehicles()
+      .subscribe((data) => {
+        this.vehicles = data;
+        console.log(data)
+        console.log(this.vehicles)
+      });
+  }
+
+  getCarsByPerson(personId: number): void {
+    this.vehicleService.getCarsByPerson(personId)
       .subscribe((data) => {
         this.vehicles = data;
         console.log(data)
