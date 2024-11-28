feacd60dce8bbe51939850ca641620963377fb85
Yi-Ji Lu <yiji85@googlemail.com>
2024-11-28 21:04:23 +0100

    remove api key

--- frontend/src/app/service/trip.service.ts
+++ frontend/src/app/service/trip.service.ts
@@ -1,11 +1,11 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Observable, of} from 'rxjs';
-import {catchError, map, tap} from 'rxjs/operators';
-import {Trip} from '../model/trip.model';
-import {FormControl} from "@angular/forms";
-import {Measurement} from "../model/measurement.model";
-import {Vehicle} from "../model/vehicle.model";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Trip } from '../model/trip.model';
+import { FormControl } from '@angular/forms';
+import { Measurement } from '../model/measurement.model';
+import { Vehicle } from '../model/vehicle.model';
 
 @Injectable({
   providedIn: 'root',
@@ -14,11 +14,10 @@ export class TripService {
   private url = 'http://localhost:8080/api/trips'; // URL to web api
 
   httpOptions = {
-    headers: new HttpHeaders({'Content-Type': 'application/json'}),
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   // getTrips(vehicleSelection: number, datePickerStart: Date | null, datePickerEnd: Date | null): Observable<Trip[]> {
   //   var queryString = `/findAll/${vehicleSelection}`;
@@ -32,13 +31,17 @@ export class TripService {
   //   );
   // }
 
-  getTrips(vehicleIds: number[], datePickerStart: Date | null, datePickerEnd: Date | null): Observable<Trip[]> {
+  getTrips(
+    vehicleIds: number[],
+    datePickerStart: Date | null,
+    datePickerEnd: Date | null
+  ): Observable<Trip[]> {
     var queryString = `/findAllByVehicleIds?`;
     vehicleIds.forEach(function (vehicleId: number) {
-      queryString += `vehicleIds=${vehicleId}&`
-    })
+      queryString += `vehicleIds=${vehicleId}&`;
+    });
     if (datePickerStart && datePickerEnd) {
-      queryString += `startTime=${datePickerStart.toISOString()}&endTime=${datePickerEnd.toISOString()}`
+      queryString += `startTime=${datePickerStart.toISOString()}&endTime=${datePickerEnd.toISOString()}`;
     }
     return this.http.get<Trip[]>(this.url + queryString).pipe(
       tap((_) => this.log('fetched trips')),
@@ -55,7 +58,7 @@ export class TripService {
   }
 
   getVehicle(tripId: number): Observable<Vehicle> {
-    return this.http.get<Vehicle>(`${this.url}/findVehiclebyTripId/${tripId}`)
+    return this.http.get<Vehicle>(`${this.url}/findVehiclebyTripId/${tripId}`);
   }
 
   getTotalDistanceForTrip(id: number): Observable<number> {
@@ -84,13 +87,18 @@ export class TripService {
     console.log(`TripService: ${message}`);
   }
 
-  getAddressFromCoordinates(latitude: number, longitude: number): Observable<Object> {
-    const google_api_key = "AIzaSyCU4bQpfJ7gExmklVSYXjCo6rv0Kxq7oV8";
-    https://maps.googleapis.com/maps/api/geocode/json?latlng=52.52389395505191,13.337066155575759&key=AIzaSyCU4bQpfJ7gExmklVSYXjCo6rv0Kxq7oV8
-      return this.http.get<Object>(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${google_api_key}`).pipe(
+  getAddressFromCoordinates(
+    latitude: number,
+    longitude: number
+  ): Observable<Object> {
+    const google_api_key = '';
+    https: return this.http
+      .get<Object>(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${google_api_key}`
+      )
+      .pipe(
         tap((_) => console.log(`fetched address`)),
         catchError(this.handleError<Object>(`fetching address`))
       );
-    ;
   }
 }
